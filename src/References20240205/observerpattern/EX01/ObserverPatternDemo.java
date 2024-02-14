b85519d10c1f23a7abcdde0947cf47ebe9e32bb6
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-02-15 03:39:07 +0530

    implement observer design pattens

---
+++ src/References20240205/observerpattern/EX01/ObserverPatternDemo.java
@@ -0,0 +1,16 @@
+package References20240205.observerpattern.EX01;
+
+public class ObserverPatternDemo {
+
+    public static void main(String[] args) {
+
+        SubjectNumber sb=new SubjectNumber();
+        new HexaObserver(sb);
+        new OctalObserver(sb);
+        new BinaryObserver(sb);
+        
+        sb.setState(10);
+        sb.notifyAllObservers();
+
+    }
+}
